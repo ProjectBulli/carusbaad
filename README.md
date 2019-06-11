2da8d250c638156f430d3e86f2c856f144e1dd83
nexoscp <nexoscp@users.noreply.github.com>
2019-06-11 08:55:47 +0200

    remove example protobuf

--- README.md
+++ README.md
@@ -7,3 +7,16 @@
    
  * build
    * `./build.release.sh`
+
+
+2019-06-08 18:15:42.775 25141-11287/? E/CAR.GAL.GAL: Received unexpected message of type 36
+2019-06-08 18:15:42.776 25141-11286/? W/CAR.GAL.GAL: ReaderThread: crashing with exception
+    java.nio.BufferOverflowException
+        at java.nio.DirectByteBuffer.put(DirectByteBuffer.java:291)
+        at oid.write(:com.google.android.gms@17455039@17.4.55 (100408-248795830):70)
+        at oia.run(:com.google.android.gms@17455039@17.4.55 (100408-248795830):5)
+2019-06-08 18:15:42.776 25141-25141/? E/CAR.SETUP.SERVICE: PROTOCOL_IO_ERROR: io error
+2019-06-08 18:15:42.777 25141-25141/? D/CAR.GAL.SNOOP: Shutdown GAL Snoop
+2019-06-08 18:15:42.777 25141-25141/? D/CAR.GAL.SNOOP: Clear the buffer
+2019-06-08 18:15:42.777 25141-25141/? D/CAR.SETUP.SERVICE: Closing fd: {ParcelFileDescriptor: java.io.FileDescriptor@7ceac9b}
+2019-06-08 18:15:44.826 17000-17074/? I/WorkerManager: dispose()
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,9 @@ use std::io;
 use libusb::{Device, DeviceDescriptor, Error, DeviceHandle};
 use std::io::{Write, Read, BufReader};
 use rustls::Stream;
+use std::net::TcpStream;
+use webpki_roots;
+use native_tls::TlsConnector;
 
 const VENDOR_GOOGLE: u16 = 0x18d1;
 const PRODUCT_ID_MIN: u16 = 0x2D00;
@@ -39,6 +42,9 @@ fn main() -> Result<(), Error> {
         .filter(None, LevelFilter::Trace)
         .init();
 
+   // tls_check();
+    //native_tls_check();
+    println!("check done");
     let context = libusb::Context::new()?;
     for device in context.devices()?.iter() {
         let device_desc = device.device_descriptor()?;
@@ -55,9 +61,9 @@ fn main() -> Result<(), Error> {
 
 fn open(device: Device, _device_desc: DeviceDescriptor) -> Result<(), Error> {
     println!("open {} {}", device.bus_number(), device.address());
+    let mut buffer: [u8; 12] = [0; 12];
     let version_package: [u8; 10] = [0, 3, 0, 6, 0, 1, 0, 1, 0, 1];
     let timeout = Duration::from_secs(10);
-    let mut buffer: [u8; 1024] = [0; 1024];
     let mut handle = device.open()?;
     let configuration = device.active_config_descriptor()?;
     for interface in configuration.interfaces() {
@@ -71,30 +77,56 @@ fn open(device: Device, _device_desc: DeviceDescriptor) -> Result<(), Error> {
         }
     }
     handle.claim_interface(0)?;
+    println!("claimed interface, write handshake data");
     handle.write_bulk(1, &version_package, timeout)?; //Direction::Out
-    let count = handle.read_bulk(129, &mut buffer, timeout)?; //Direction::In
+    println!("waiting for handshake response");
+   // let count = handle.read_bulk(129, &mut buffer, timeout)?; //Direction::In
     //[0, 3, 0, 8, 0, 2, 0, 1, 0, 5, 0, 0]
-    //println!("{} {:?}", count, buffer);
+   // println!("read {} {:?}", count, buffer);
+    //connectRusTLS(timeout, handle);
+    connectNativeTLS(timeout, handle);
+
+    Ok(())
+}
+
+fn connectRusTLS(timeout: Duration, mut handle: DeviceHandle) {
+    let mut buffer: [u8; 1024] = [0; 1024];
     let mut config = rustls::ClientConfig::new();
-    load_key_and_cert(&mut config);
-    let rc_config = Arc::new(config);
-    let example_com = webpki::DNSNameRef::try_from_ascii_str("example.com").unwrap();
-    let mut client = rustls::ClientSession::new(&rc_config, example_com);
-    let mut socket = USBBulkStream::new ( 129,  1, timeout, handle );//TODO this must be the usb connection
-    let mut s = Stream::new ( &mut client,  &mut socket);
+    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+//load_key_and_cert(&mut config);
+    let dns_name = webpki::DNSNameRef::try_from_ascii_str("www.google.com").unwrap();
+    let mut client = rustls::ClientSession::new(&Arc::new(config), dns_name);
+    let mut socket = USBBulkStream::new(129, 1, timeout, handle);
+//TODO this must be the usb connection
+    let mut stream = Stream::new(&mut client, &mut socket);
     println!("first contact");
-    let read_result = s.read(&mut buffer);
+    let read_result = stream.read(&mut buffer);
     match read_result {
         Err(E) => println!("Error reading {:?}", E),
         Ok(size) => println!("successful read {} bytes", size)
     }
     println!("read done");
-    //0 3 0 8 2 0 1 0 5
-   // println!("first read {}", hex::encode(buffer));
-    s.write("foo".as_bytes());
+//0 3 0 8 2 0 1 0 5
+// println!("first read {}", hex::encode(buffer));
+    stream.write("foo".as_bytes());
     println!("first write");
+}
 
-    Ok(())
+fn connectNativeTLS(timeout: Duration, mut handle: DeviceHandle) {
+  //  let mut buffer: [u8; 1024] = [0; 1024];
+    let mut socket = USBBulkStream::new(129, 1, timeout, handle);
+ //   let mut tls = TlsConnector::builder().use_sni(false).build().unwrap();
+ //   tls.connect("google.com", socket).unwrap();
+    let tls = TlsConnector::new().unwrap();
+    let mut s = tls.connect("google.com", socket);
+    match &mut s {
+        Ok(stream) => {
+            let mut res = vec![];
+            stream.read_to_end(&mut res);
+            println!("{}", String::from_utf8_lossy(&res));
+        }
+        Err(_) => {}
+    }
 }
 
 fn load_certs() -> Vec<rustls::Certificate> {
@@ -113,4 +145,37 @@ fn load_key_and_cert(config: &mut rustls::ClientConfig) {
     let privkey = load_private_key();
 
     config.set_single_client_cert(certs, privkey);
+}
+
+fn tls_check() {
+    let mut buffer: [u8; 1024] = [0; 1024];
+    let mut config = rustls::ClientConfig::new();
+    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+    //load_key_and_cert(&mut config);
+    let dns_name = webpki::DNSNameRef::try_from_ascii_str("www.google.com").unwrap();
+    let mut client = rustls::ClientSession::new(&Arc::new(config), dns_name);
+    //let mut socket = USBBulkStream::new ( 129,  1, timeout, handle );//TODO this must be the usb connection
+    let mut socket = TcpStream::connect("www.google.com:443").unwrap();
+    let mut stream = Stream::new ( &mut client,  &mut socket);
+    println!("first contact");
+    stream.write(concat!("GET / HTTP/1.1\r\n",
+                      "Host: google.com\r\n",
+                      "Connection: close\r\n",
+                      "Accept-Encoding: identity\r\n",
+                      "\r\n")
+        .as_bytes())
+        .unwrap();
+    let read_result = stream.read(&mut buffer);
+}
+
+fn native_tls_check() {
+    let connector = TlsConnector::new().unwrap();
+
+    let stream = TcpStream::connect("google.com:443").unwrap();
+    let mut stream = connector.connect("google.com", stream).unwrap();
+
+    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+    let mut res = vec![];
+    stream.read_to_end(&mut res).unwrap();
+    println!("{}", String::from_utf8_lossy(&res));
 }
